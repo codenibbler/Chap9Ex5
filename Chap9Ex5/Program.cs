c1708bde1d4cc70ce483963701cf0541792b0e74
Brian Banahan <brianbanahan@gmail.com>
2018-08-09 15:12:39 +0100

    Logic complete. Does not test if position is at terminus of array, exception thrown in this case.

--- Chap9Ex5/Program.cs
+++ Chap9Ex5/Program.cs
@@ -10,6 +10,23 @@ namespace Chap9Ex5
     {
         static void Main(string[] args)
         {
+            // Write a method that checks whether an element, from a certain position
+            // in an array is greater than its two neighbors. Test whether the
+            // method works correctly.
+            int[] testArr = { 1, 2, 3, 4, 2, 7, 5, 2, 3 };
+            int position = 5;
+            Console.WriteLine(IsElementGreater(testArr, position));
+            Console.ReadLine();
+
+        }
+
+        static bool IsElementGreater(int[] inputArr, int position)
+        {
+            if (inputArr[position] > inputArr[position - 1] && inputArr[position] > inputArr[position + 1])
+            {
+                return true;
+            }
+            else return false;
         }
     }
 }
